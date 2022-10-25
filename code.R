fdde55273a9b8fe7ddb0c3d6b424132d45e5ce3e
Anders <ansor18@student.sdu.dk>
2022-10-25 13:19:46 +0200

    added capital flagging

--- code.R
+++ code.R
@@ -1,9 +1,23 @@
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
+library(stringr)
 
 dat <- read_csv("data.csv")
 dat <- dat %>% select(c("countryName","eprtrSectorName","facilityName","Longitude","Latitude","City","pollutant","emissions","reportingYear"))
+cap <- read_csv("capitals.csv")
+cap <- cap %>% mutate(country = countryName)
+
+# Flag capitals
+dat <- add_column(dat, isCapital = "FALSE", .after = "City")
+dat <- dat %>%
+  left_join(cap) %>%
+  mutate(isCapital = (City == capital | (countryName == country & str_detect(City, capitalInLocal)))) %>%
+  select(-c(capital, capitalInLocal, country))
+
+#capitals <- dat %>% 
+#    filter(isCapital == TRUE) %>%
+#    distinct(countryName, City, isCapital)
 
 #samp <- sample_n(dat, 50)
 
