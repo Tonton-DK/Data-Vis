8417324624500b29cd5d88312c8cd247e8a9d1ea
CoffeeOverload <ansor18@student.sdu.dk>
2022-11-07 16:05:04 +0100

    update q6 - add sorting

--- q6.R
+++ q6.R
@@ -5,9 +5,10 @@ library(dplyr)
 library(plotly)
 
 q6_ui <- tabPanel("Question 6",
-                  radioButtons("charttype", "Select chart type:",
-                               c("Double axis" = "daxis",
-                                 "Grouped" = "groupd")),
+                  radioButtons("orderby", "Order by:",
+                               c("Capital" = "cap",
+                                 "Non-capital" = "ncap",
+                                 "Country")),
                   plotlyOutput("pollutionPlot6")
 )
 
@@ -17,28 +18,28 @@ q6_server <- function(input, output){
       summarise(mean_emission = mean(emissions, na.rm=TRUE)) %>%
       mutate(mean_emission = mean_emission / 10^6,
              isCapital = ifelse(isCapital, "Capital", "Non-capital"))
-    
-    if(input$charttype == "daxis") {
-      brk = c(-400, -300, -200, -100, 0, 100, 200, 300, 400)
-      lbl = c(400, 300, 200, 100, 0, 100, 200, 300, 400)
-      q6alt <- mutate(q6, mean_emission = ifelse(isCapital == "Capital", mean_emission, mean_emission * -1))
-      ggply <- ggplotly(ggplot(q6alt, aes(x = countryName, y = mean_emission, fill = isCapital)) +
-        geom_col() +
-        scale_y_continuous(limit = c(-400, 400), breaks = brk, labels = lbl, expand = c(0, 0)) +
-        labs(x = "Country", y = "Mean emission (thousand tons)", fill = "Pollution in capital") +
-        theme(axis.text.x = element_text(angle = 55, hjust = 1, vjust = 0.5))
-      )
+    q6 <- q6 %>% mutate(mean_emission = ifelse(isCapital == "Capital", mean_emission, mean_emission * -1),
+                        order_cap = ifelse(isCapital == "Capital", mean_emission, 0),
+                        order_ncap = ifelse(isCapital == "Capital", 0, mean_emission))
+    #q6 <- q6 %>% arrange(desc(mean_emission))
+    lim = c(-400, 400)
+    brk = c(-400, -300, -200, -100, 0, 100, 200, 300, 400)
+    lbl = c(400, 300, 200, 100, 0, 100, 200, 300, 400)
+    if(input$orderby == "cap") {
+      plt <-ggplot(q6, aes(x = reorder(countryName, order_cap, decreasing = T), y = mean_emission, fill = isCapital))
+    }
+    else if (input$orderby == "ncap") {
+      plt <- ggplot(q6, aes(x = reorder(countryName, order_ncap, decreasing = F, sum, order = T), y = mean_emission, fill = isCapital))
     }
     else {
-      q6 <- mutate(q6, mean_emission = ifelse(mean_emission > 0, mean_emission, 0))
-      ggply <- ggplotly(ggplot(q6, aes(x = countryName, y = mean_emission, fill = isCapital)) +
-        geom_bar(stat = "identity", position = "dodge") +
-        scale_y_continuous(limit = c(0, 400), expand = c(0, 0)) +
-        labs(x = "Country", y = "Mean emission (thousand tons)", fill = "Pollution in capital") +
-        theme(axis.text.x = element_text(angle = 55, hjust = 1, vjust = 0.5))
-      )
+      plt <- ggplot(q6, aes(x = countryName, y = mean_emission, fill = isCapital))
     }
-    
+    ggply <-ggplotly(plt +
+                       geom_bar(stat='identity') +
+                       scale_y_continuous(limit = c(-400, 400), breaks = brk, labels = lbl, expand = c(0, 0)) +
+                       labs(x = "Country", y = "Mean emission (thousand tons)", fill = "Pollution in capital") +
+                       theme(axis.text.x = element_text(angle = 55, hjust = 1, vjust = 0.5))
+    )
     ggply
   })
 }
