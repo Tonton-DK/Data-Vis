40b09571b866ca8a63935878124cbc75ce9fc38c
CoffeeOverload <ansor18@student.sdu.dk>
2022-11-13 16:59:45 +0100

    fix missing axis titles

--- q5.R
+++ q5.R
@@ -3,10 +3,10 @@ library(ggplot2)
 library(dplyr)
 library(plotly)
 
-q5_ui <- tabPanel(  
+q5_ui <- tabPanel(
   "Question 5",
-  plotlyOutput("pollutionPlot5", 
-    width = "1200px", 
+  plotlyOutput("pollutionPlot5",
+    width = "1200px",
     height = "800px")
 )
 
@@ -14,17 +14,17 @@ q5_server <- function(input, output){
   renderPlotly({
     plt1 <- create_q5_plot(q5)
     plt2 <- create_q5_plot(q5alt)
-    subplot(plt1, plt2, nrows = 2)
+    subplot(plt1, plt2, nrows = 2, shareX = TRUE, shareY = TRUE)
   })
 }
 
 create_q5_plot <- function(df) {
   plt <- ggplotly(ggplot(
-    df, 
+    df,
     aes(
       x = reportingYear,
-      y = mean_emission, 
-      group = pollutant, 
+      y = mean_emission,
+      group = pollutant,
       color = pollutant,
       text = paste(
         "Reporting year:\t\t", reportingYear,
@@ -34,9 +34,9 @@ create_q5_plot <- function(df) {
     geom_point() +
     geom_line() +
     labs(
-      x = "Reporting year", 
-      y = "Mean emission (1000x tons)", 
-      color = "Pollutant"), 
+      x = "Reporting year",
+      y = "Mean emission (1000x tons)",
+      color = "Pollutant"),
     tooltip = c("text")
   ) %>%
     config(displaylogo = FALSE)
@@ -44,13 +44,13 @@ create_q5_plot <- function(df) {
 }
 
 load_q5_data <- function() {
-  top2 <- dat %>% 
+  top2 <- dat %>%
     group_by(pollutant) %>%
     summarise(temp = mean(emissions, na.rm=TRUE)) %>%
     arrange(temp, .by_group = TRUE) %>%
     top_n(2) %>%
     pull(pollutant)
-  temp <- dat %>% 
+  temp <- dat %>%
     group_by(pollutant, reportingYear) %>%
     summarise(mean_emission = sum(emissions, na.rm=TRUE)) %>%
     mutate(
